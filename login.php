cc219f63ba0512d7496419a70a08b5ac0ec404e7
f-s-plus-plus <fsaulean@uvm.edu>
2018-12-24 21:11:01 -0500

    first commit; a lot of work needs to be done

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html
+  <?php
+    include 'head.php';
+  ?>
+  <body>
+    <div class="container-login">
+      <div id="login-form">
+        <?php
+          include 'login_form.php';
+        ?>
+      </div>
+    </div>
+  </body>
+</html>
